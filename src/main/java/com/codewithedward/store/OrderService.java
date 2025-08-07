4c41bdc6686bd033785c546cbeda347c76b8c8a7
Edward Mati <memwendwa@gmail.com>
2025-08-07 12:21:47 +0300

    Initial commit

---
+++ src/main/java/com/codewithedward/store/OrderService.java
@@ -0,0 +1,26 @@
+package com.codewithedward.store;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
+
+@Service
+public class OrderService {
+
+    private PaymentService paymentService;
+
+    public  OrderService() {}
+
+    @Autowired
+   public OrderService(PaymentService paymentService) {
+      this.paymentService = paymentService;
+   }
+
+    public void placeOrder()  {
+        paymentService.processPayment(10);
+    }
+
+    public void setPaymentService(PaymentService paymentService) {
+        this.paymentService = paymentService;
+    }
+}
---
+++ src/main/java/com/codewithedward/store/PaymentService.java
@@ -0,0 +1,5 @@
+package com.codewithedward.store;
+
+public interface PaymentService {
+    void processPayment(double amount);
+}
